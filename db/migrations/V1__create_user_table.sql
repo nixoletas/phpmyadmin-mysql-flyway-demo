b9148f043366d3b1d9cd3adf213a175e8a717bb7
nixoletas <nicholetas@gmail.com>
2024-11-03 01:43:08 -0400

    first commit

---
+++ db/migrations/V1__create_user_table.sql
@@ -0,0 +1,6 @@
+-- V1__create_nome_idade_table.sql
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    age INT NOT NULL
+);
---
+++ db/migrations/V2__create_test_user.sql
@@ -0,0 +1,2 @@
+-- V2__insert_teste_user.sql
+INSERT INTO users (name, age) VALUES ('Nick Miyasato', 26);
